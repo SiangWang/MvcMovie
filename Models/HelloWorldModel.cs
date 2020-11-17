092cc24a9bcd12eb9bc631a32cf8ad788c5ebe22
Siang <ssiangww@gmail.com>
2020-11-17 17:22:57 +0800

    first commit

---
+++ Models/HelloWorldModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMovie.Models
+{
+    public class HelloWorldModel
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+    }
+}
